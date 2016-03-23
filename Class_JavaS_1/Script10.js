fa5164afcac9ed5670940a81187ca0ec048636ee
keegaz <keegazh@gmail.com>
2016-03-23 14:05:36 +0000

    Class 4 classwork

--- Class_JavaS_1/Script10.js
+++ Class_JavaS_1/Script10.js
@@ -1,5 +1,10 @@
+
+var b = 0;
 for (var i = 1; i<=100; i++ )
 {
-    document.write(i);
-    
+    document.write("<br>", i);
+    b = b + i;
+          
 }
+
+document.write("The total of 1-100 is: ", b);
---
+++ EmptySite/Script1.js
@@ -0,0 +1,4 @@
+for ( var i = 0; i >=15; i++)
+{
+    document.write(i);
+}
